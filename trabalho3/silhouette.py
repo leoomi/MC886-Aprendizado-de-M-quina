9ce6b5d4cd646e46810f3fe05f69b87530185584
Joao Lopes <jpedrorl@gmail.com>
2017-11-09 17:22:12 -0200

    Trabalho 3 - Initial kmeans tryout

---
+++ trabalho3/silhouette.py
@@ -0,0 +1,57 @@
+
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
+
+# Generating the sample data from make_blobs
+# This particular setting has one distinct cluster and 3 clusters placed close
+# together.
+# X, y = make_blobs(n_samples=500,
+#                   n_features=2,
+#                   centers=4,
+#                   cluster_std=1,
+#                   center_box=(-10.0, 10.0),
+#                   shuffle=True,
+#                   random_state=1)  # For reproducibility
+
+# read data
+print("Reading data")
+data_path = "documents/data.csv"
+data_file = open(data_path)
+data = data_file.readlines()[:10000]
+data = [list(map(str.strip, i.split(","))) for i in data]
+data = [list(map(float, i)) for i in data]
+X = np.array(data)
+print("done")
+
+# range_n_clusters = [50, 100, 150, 200, 250]
+
+for n_clusters in range(125, 176, 1):
+    # Create a subplot with 1 row and 2 columns
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(18, 7)
+
+    # The 1st subplot is the silhouette plot
+    # The silhouette coefficient can range from -1, 1 but in this example all
+    # lie within [-0.1, 1]
+    ax1.set_xlim([-0.1, 1])
+    # The (n_clusters+1)*10 is for inserting blank space between silhouette
+    # plots of individual clusters, to demarcate them clearly.
+    ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+
+    # Initialize the clusterer with n_clusters value and a random generator
+    # seed of 10 for reproducibility.
+    clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+    cluster_labels = clusterer.fit_predict(X)
+
+    # The silhouette_score gives the average value for all the samples.
+    # This gives a perspective into the density and separation of the formed
+    # clusters
+    silhouette_avg = silhouette_score(X, cluster_labels)
+    print("For n_clusters =", n_clusters,
+          "The average silhouette_score is :", silhouette_avg)
