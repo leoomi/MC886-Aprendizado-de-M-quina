44bf3f57c61673918a15ac03f405d2a2ccb5febc
Leo Omi <leoyuuki@gmail.com>
2017-11-09 23:52:27 -0200

    hierarchical clustering info added

--- trabalho3/kmeansPCA.py
+++ trabalho3/kmeansPCA.py
@@ -1,20 +1,23 @@
 
-#import
 print("Importing libraries")
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
 
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
-print("done")
 
-# read data
+
 print("Reading data")
 data_path = "documents/data.csv"
 data_file = open(data_path)
-data = data_file.readlines()
+data = data_file.readlines()[:10000]
 data = [list(map(str.strip, i.split(","))) for i in data]
 data = [list(map(float, i)) for i in data]
-data = np.array(data)
+X = np.array(data)
 print("done")
 
 print("Starting PCA")
@@ -25,9 +28,7 @@ data = pca.transform(data)
 print(data.shape)
 print("PCA finished")
 
-# compute kmeans
-print("Computing k-means")
-kmeans = KMeans(n_clusters=151, random_state=0, verbose=False, max_iter=10, n_init=3).fit(data)
-print(kmeans.inertia_)
-
-print("done");
+clusterer = KMeans(n_clusters=151, random_state=10)
+cluster_labels = clusterer.fit_predict(X)
+silhouette_avg = silhouette_score(X, cluster_labels)
+print("Score: ", silhouette_avg)
